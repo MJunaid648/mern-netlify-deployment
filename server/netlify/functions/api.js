66d6d0eabe88cf841110f62389e8ae1987f33476
Muhammad Junaid <648mjunaid@gmail.com>
2025-02-04 23:08:09 +0500

    toml

--- server/netlify/functions/api.js
+++ server/netlify/functions/api.js
@@ -1,4 +1,3 @@
-import cors from "cors";
 import express, { Router } from "express";
 import { getUploadURL } from "../../controllers/uploadData.controller.js";
 import ServerlessHttp from "serverless-http";
@@ -7,12 +6,11 @@ const app = express();
 const router = Router();
 app.use(express.json());
 
-app.use(cors());
 router.get("/getPresignedURL", getUploadURL);
-
 router.get("/test", (req, res) => {
   res.send("Hello World");
 });
 
-app.use("/api", router);
+
+app.use("/api",router);
 export const handler = ServerlessHttp(app);
